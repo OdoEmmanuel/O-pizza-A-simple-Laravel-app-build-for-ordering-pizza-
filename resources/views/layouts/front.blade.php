35a4519bf1b7ea553f19e05945bcc9cc5a49784e
Emmanuel <Emmanuel@localhost.localdomain>
2020-07-01 13:29:48 +0100

    updated composer

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -78,7 +78,6 @@
                              <li>
                                 <a class="dropdown-item" href="{{ url('/logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"> Logout
                                 </a>
-                        
                                 <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
                                     {{ csrf_field() }}
                                 </form>
